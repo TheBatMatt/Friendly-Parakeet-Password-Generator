bf2d46a92f39f4a6b7303472f1aa0699024c0e20
Ted J <tedmjablonowski@gmail.com>
2022-02-16 06:31:58 -0500

    Number and Symbols functions completed

--- Develop/script.js
+++ Develop/script.js
@@ -36,15 +36,11 @@ var passwordSymbols
 /* Generate Password  */
 
 function getPasswordSize(){ 
-
-  //Confirm Length
   var lengthPrompt = window.prompt('How long do you want your password to be? Please choose between 8 and 128 characters.');
-
   if (lengthPrompt === "" || lengthPrompt === "null" || isNaN(lengthPrompt) || lengthPrompt < 8 || lengthPrompt > 128 ) {
     window.alert("You need to provide a valid answer! Please try again.");
-    return
+    return getPasswordSize();
   }
-  
   return lengthPrompt;
 }
 
@@ -52,39 +48,41 @@ function getPasswordSize(){
 
 function getPasswordCase(){
   var casePrompt = window.prompt('Do you want to include upper and lower case letters? Default is uppercase.');
-
   if (casePrompt === "" || casePrompt === "null" || typeof casePrompt !== 'string' || (casePrompt.toLowerCase() !== 'yes' && casePrompt.toLowerCase() !== 'no')) {
-    console.log(casePrompt);
     window.alert("You need to provide a valid answer! Please try again.");
-    return;
+    return getPasswordCase();
   }
-
   return casePrompt.toLowerCase();
-}
+};
 
 // Password Number
 
 function getPasswordNumber(){
   var numberPrompt = window.prompt('Do you want to include numbers?');
-
-  if (passwordNumber === "" || passwordNumber === "null") {
+  if (numberPrompt  === "" || numberPrompt  === "null" || typeof numberPrompt  !== 'string' || (numberPrompt.toLowerCase() !== 'yes' && numberPrompt.toLowerCase() !== 'no')) {
     window.alert("You need to provide a valid answer! Please try again.");
+    return getPasswordNumber();
   }
-}
+  return numberPrompt.toLowerCase();
+};
 
 // Password Symbol
 
 function getPasswordSymbol(){
   var symbolPrompt = window.prompt ('Do you want to include symbols?')
-
-  if (passwordSymbol === "" || passwordSymbol === "null") {
+  if (symbolPrompt  === "" || symbolPrompt  === "null" || typeof symbolPrompt  !== 'string' || (symbolPrompt.toLowerCase() !== 'yes' && symbolPrompt.toLowerCase() !== 'no')) {
     window.alert("You need to provide a valid answer! Please try again.");
+    return getPasswordSymbol();
   }
-}
+  return symbolPrompt.toLowerCase();
+};
+
 
 // Password Generator
 
 function generatePassword (){
   passwordSize = getPasswordSize();
   passwordCase = getPasswordCase();
-}
+  passwordNumber = getPasswordNumber();
+  passwordSymbol = getPasswordSymbol();
+};
